02820c488a2ba3d234bce9b051b9f6ee91ca004c
abhay das <131251345+Abhay-2241@users.noreply.github.com>
2024-03-19 00:44:31 +0530

    added admin page and ip box

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+
+import { Admin as Admin1 } from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/input-box";
+export default function Admin() {
+    return (
+        <div>
+            <h1> Admin  </h1>
+            <Admin1/>
+            <InputBox/>
+        </div>
+    );
+}
